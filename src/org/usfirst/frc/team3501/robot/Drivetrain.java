f064aa07749d1797e9a8fd1a8d0a3a658cc89572
Logan Howard <logan@oflogan.com>
2016-03-16 17:18:05 -0700

    add chezy drive algorithm

--- src/org/usfirst/frc/team3501/robot/Drivetrain.java
+++ src/org/usfirst/frc/team3501/robot/Drivetrain.java
@@ -6,20 +6,18 @@ import java.util.stream.Stream;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Drivetrain {
 
     private CANTalon frontLeft, frontRight, rearLeft, rearRight;
 
-    private RobotDrive robotDrive;
     private DoubleSolenoid leftShifter, rightShifter;
 
     private DoubleSolenoid.Value shifterState;
 
     private boolean flipped;
 
-    private double lastLeft, lastRight;
+    double oldTurn, quickStopAccumulator;
 
     public Drivetrain() {
         frontLeft  = new CANTalon(FRONT_LEFT_ADDR);
@@ -27,46 +25,139 @@ public class Drivetrain {
         rearLeft   = new CANTalon(REAR_LEFT_ADDR);
         rearRight  = new CANTalon(REAR_RIGHT_ADDR);
 
-        robotDrive = new RobotDrive(frontLeft,  rearLeft,
-                                    frontRight, rearRight);
-
         leftShifter  = new DoubleSolenoid(PCM_B, 3, 6);
         rightShifter = new DoubleSolenoid(PCM_B, 2, 7);
 
         shifterState = LOW_GEAR;
         setShifters(shifterState);
-        coast();
+        brake();
         flipped = false;
     }
 
-    public void drive(double left, double right) {
-        if (Math.abs(left) < MIN_DRIVE_JOYSTICK_INPUT)
-            left = 0;
-        if (Math.abs(right) < MIN_DRIVE_JOYSTICK_INPUT)
-            right = 0;
+    public void drive(double forward, double turn) {
+        if (Math.abs(forward) < MIN_DRIVE_JOYSTICK_INPUT)
+            forward = 0;
+        if (Math.abs(turn) < MIN_DRIVE_JOYSTICK_INPUT)
+            turn = 0;
 
         double powerCoeff = (shifterState == LOW_GEAR)
                                 ? LOW_GEAR_POWER_COEFF
                                 : HIGH_GEAR_POWER_COEFF;
 
-        if (flipped) { // swap left and right, and reverse inputs
-            double temp = left;
-            left  = -right;
-            right = -temp;
+        if (flipped) {
+            forward *= -1;
+            turn *= -1;
         }
 
-        // slap a running average on the ouputs
-        left = (left + 7 * lastLeft) / 8;
-        lastLeft = left;
+        forward *= powerCoeff;
 
-        right = (right + 7 * lastRight) / 8;
-        lastRight = right;
+        // begin code from github.com/Team254/FRC-2014
+        double wheelNonLinearity;
 
-        driveRaw(powerCoeff * left, powerCoeff * right);
+        double negInertia = turn - oldTurn;
+        oldTurn = turn;
+
+        if (shifterState == HIGH_GEAR) {
+            wheelNonLinearity = 0.6;
+
+            // Apply a sin function that's scaled to make it feel better.
+            turn = Math.sin(Math.PI / 2.0 * wheelNonLinearity * turn)
+                   / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+            turn = Math.sin(Math.PI / 2.0 * wheelNonLinearity * turn)
+                   / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+        } else {
+            wheelNonLinearity = 0.5;
+
+            // Apply a sin function that's scaled to make it feel better.
+            turn = Math.sin(Math.PI / 2.0 * wheelNonLinearity * turn)
+                   / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+            turn = Math.sin(Math.PI / 2.0 * wheelNonLinearity * turn)
+                   / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+            turn = Math.sin(Math.PI / 2.0 * wheelNonLinearity * turn)
+                   / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+        }
+
+        double leftPwm, rightPwm, overPower;
+        double sensitivity;
+
+        double angularPower;
+        double linearPower;
+
+        double negInertiaAccumulator = 0.0;
+        double negInertiaScalar;
+        if (shifterState == HIGH_GEAR) {
+            negInertiaScalar = 5.0;
+            sensitivity = 0.75; // arbitrary
+        } else {
+            if (turn * negInertia > 0) {
+                negInertiaScalar = 2.5;
+            } else {
+                if (Math.abs(turn) > 0.65) {
+                    negInertiaScalar = 5.0;
+                } else {
+                    negInertiaScalar = 3.0;
+                }
+            }
+
+            sensitivity = 0.75; // arbitrary
+        }
+
+        double negInertiaPower = negInertia * negInertiaScalar;
+        negInertiaAccumulator += negInertiaPower;
+
+        turn += negInertiaAccumulator;
+        if (negInertiaAccumulator > 1) {
+            negInertiaAccumulator -= 1;
+        } else if (negInertiaAccumulator < -1) {
+            negInertiaAccumulator += 1;
+        } else {
+            negInertiaAccumulator = 0;
+        }
+
+        linearPower = forward;
+
+        // ignoring quickturn
+        overPower = 0.0;
+        angularPower = Math.abs(forward) * turn * sensitivity
+                       - quickStopAccumulator;
+
+        if (quickStopAccumulator > 1) {
+            quickStopAccumulator -= 1;
+        } else if (quickStopAccumulator < -1) {
+            quickStopAccumulator += 1;
+        } else {
+            quickStopAccumulator = 0.0;
+        }
+        // end ignoring quickturn
+
+        rightPwm = leftPwm = linearPower;
+        leftPwm  += angularPower;
+        rightPwm -= angularPower;
+
+        if (leftPwm > 1.0) {
+            rightPwm -= overPower * (leftPwm - 1.0);
+            leftPwm = 1.0;
+        } else if (rightPwm > 1.0) {
+            leftPwm -= overPower * (rightPwm - 1.0);
+            rightPwm = 1.0;
+        } else if (leftPwm < -1.0) {
+            rightPwm += overPower * (-1.0 - leftPwm);
+            leftPwm = -1.0;
+        } else if (rightPwm < -1.0) {
+            leftPwm += overPower * (-1.0 - rightPwm);
+            rightPwm = -1.0;
+        }
+        // end code from github.com/Team254/FRC-2014
+
+        driveRaw(leftPwm, rightPwm);
     }
 
     public void driveRaw(double left, double right) {
-        robotDrive.tankDrive(left, right, false);
+        frontLeft.set(left);
+        rearLeft.set(left);
+
+        frontRight.set(-right);
+        rearRight.set(-right);
     }
 
     public void stop() {
